30d03d8678ee17ec9af28ebb462557f2dc49f804
RaulMalawene <rauljoaomalawene@gmail.com>
2026-06-17 17:24:53 +0200

    fd

--- resources/js/components/AppFooter.vue
+++ resources/js/components/AppFooter.vue
@@ -48,7 +48,7 @@
     </div>
 
     <div class="footer-bottom">
-      © 2025 BIOFUND. Todos os direitos reservados. Inspirado na conservação ambiental de Moçambique.
+      © 2025 BIOFUND. Todos os direitos reservados.
     </div>
   </footer>
 </template>
