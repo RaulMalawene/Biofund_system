e144aa49e7e7723cf138610f52f95ffc4ef5cc78
RaulMalawene <rauljoaomalawene@gmail.com>
2026-07-13 13:08:59 +0200

    ALTERA

--- resources/js/views/funcionario externo/SubmeterReclamacaoView.vue
+++ resources/js/views/funcionario externo/SubmeterReclamacaoView.vue
@@ -7,10 +7,6 @@
       <!-- HEADER -->
       <div class="page-header">
         <div class="page-badge">
-          <svg width="14" height="14" viewBox="0 0 32 32" fill="none">
-            <path d="M6 26C6 26 8 14 20 10C28 7 28 4 28 4C28 4 30 16 20 20C12 24 10 28 10 28" fill="#2D6A4F" />
-            <path d="M10 28C10 28 14 20 20 20" stroke="#1B4332" stroke-width="2" stroke-linecap="round" />
-          </svg>
           BIOFUND
         </div>
         <h1>Registar Ocorrência</h1>
@@ -308,13 +304,7 @@
       <!-- SUBMIT BANNER -->
       <div class="submit-banner">
         <div class="submit-banner-text">
-          <div class="submit-banner-icon">
-            <svg width="18" height="18" viewBox="0 0 32 32" fill="none">
-              <path d="M6 26C6 26 8 14 20 10C28 7 28 4 28 4C28 4 30 16 20 20C12 24 10 28 10 28" fill="#52B788" />
-              <path d="M10 28C10 28 14 20 20 20" stroke="#D8F3DC" stroke-width="2" stroke-linecap="round" />
-            </svg>
-          </div>
-          <p>Ao submeter esta ocorrência, você declara que as informações prestadas são verdadeiras e autoriza a Biofund
+          <p>Ao submeter esta ocorrência, você declara que as informações prestadas são verdadeiras e autoriza a BIOFUND
             a utilizá-las para fins de investigação ambiental.</p>
         </div>
         <button class="btn-submit" @click="submitForm" :disabled="submitting">
@@ -339,7 +329,7 @@
         </div>
 
         <h3>Ocorrência Enviada!</h3>
-        <p>A sua ocorrência foi registada com sucesso. A equipa da Biofund irá analisar e tomar as medidas necessárias.</p>
+        <p>A sua ocorrência foi registada com sucesso. A equipa da  BIOFUND irá analisar e tomar as medidas necessárias.</p>
 
         <!-- Tracking Code -->
         <div class="tracking-box" v-if="trackingCode">
